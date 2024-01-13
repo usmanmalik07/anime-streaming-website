f1c2a7b1c23e38da2146de9d53c0c435ae776ba6
Usman Malik <usmanmalikk2004@gmail.com>
2024-01-13 20:42:44 +0500

    1

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,17 @@ use App\Http\Controllers\AnimeController;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+
 Route::get('/',[FrontController::class,'index'])->name('front.home');
 Route::get('/Shop',[userctrl::class,'showShop'])->name('shop');
-
 Route::get('/aboutus',[FrontController::class,'aboutus'])->name('front.aboutus');
-<<<<<<< HEAD
-
-=======
 Route::get('/contact',[FrontController::class,'contact'])->name('front.contact');
-// Route::get('/', function () {
-//     return view('login');
-// });
->>>>>>> e780b81f383b3c317409763b1f41c39580e30b79
 Route::get('/login',[userctrl::class,'indexlogin'])->name('login');
 Route::get('/signup',[userctrl::class,'signup'])->name('signup');
-Route::get('/signin',[userctrl::class,'signin'])->name('userlogin');
-
-Route::post('/userlogin', 'App\Http\Controllers\userctrl@userlogin');
-Route::get('/usershop', 'App\Http\Controllers\userctrl@viewshop');
-
-
 Route::post('/signup', 'App\Http\Controllers\userctrl@store');
-
 Route::get('/dashboard', 'App\Http\Controllers\userctrl@viewdashboard');
 Route::get('/logout', 'App\Http\Controllers\userctrl@logout');
 Route::post('/login', 'App\Http\Controllers\userctrl@login');
-// Route::get('/login', 'App\Http\Controllers\userctrl@indexlogin');
 Route::get('/dashboard', [userctrl::class, 'printName'])->name('animename');
 Route::post('/add-name', [userctrl::class, 'addName'])->name('add.name');
 Route::delete('/remove-name/{id}', [userctrl::class, 'removeName'])->name('remove.name');
@@ -52,7 +34,6 @@ Route::get('/printItem', [userctrl::class, 'printItem'])->name('print.item');
 Route::post('/add-Item', [userctrl::class, 'addItem'])->name('add.Item');
 Route::delete('/remove-Item/{id}', [userctrl::class, 'removeName'])->name('remove.Item');
 Route::get('/search', [userctrl::class, 'search'])->name("search.name");
-// Route::get('/shop', [userctrl::class, 'showShop'])->name('shop.show');
 Route::post('/shop/addShop', [userctrl::class, 'addItemShop'])->name('shop.addItemShop');
 Route::delete('/shop/remove/{id}', [userctrl::class, 'removeItemShop'])->name('shop.removeItemShop');
 Route::get('/usershop',[userctrl::class,'showuserShop'])->name('front.usershop');
